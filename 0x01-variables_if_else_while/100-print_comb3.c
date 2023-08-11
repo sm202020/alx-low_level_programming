afdd0b219a0ab91ef0b6d74f5d4f9211ed2f61b1
Sunday Mebawondu <akinbode4life@gmail.com>
2023-08-11 20:55:16 +0000

    9-print_comb.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,24 +6,26 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-	{
-	int i;
-	int j;
+{
+int i;
+int j;
 
 
-	for (i = 48; i <= 56; i++)
-	{
-		for (j = i + 1; j <= 57; j++)
-			{
-			putchar(i);															    				putchar(j);
-																							if (i == 56 && j == 57)																			continue;
-																								putchar(',');
-																								putchar(' ');
+for (i = 48; i <= 56; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+
+putchar(i);
+putchar(j);
+if (i == 56 && j == 57)
+continue;
+putchar(',');
+putchar(' ');
 																							}
-	}
-	putchar('\n');
-	return (0);
-	}
-
+}
+putchar('\n');
+return (0);
+}
 
 ~
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 
-	{
-	int a;
+{
+int a;
 
-	for (a = 0; a < 10; a++)
-	{
-		putchar(a + '0');
-		if (a < 9)
-	         {
-			putchar(',');
-		      	putchar(' ');
+for (a = 0; a < 10; a++)
+{
+putchar(a + '0');
+if (a < 9)
+{
+putchar(',');
+putchar(' ');
 		 }
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
