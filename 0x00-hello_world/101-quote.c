a552d0274f4a82769877ba477adf7cbba5bd66ea
Sunday Mebawondu <akinbode4life@gmail.com>
2023-08-11 05:02:50 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
 
 /**
- * main - Starting Entry
- *         .    
+ * main - Prints exactly "and that piece of art is useful" - Dora Korpar,
+ * 2015-10-19, followed by a new line, to the standard error.
+ *    
  * Return: Always 1.
  */
 int main(void)
