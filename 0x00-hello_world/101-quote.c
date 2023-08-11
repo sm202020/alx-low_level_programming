39e0781f6a6e06b552cc8c423d7a3e69af472652
Sunday Mebawondu <akinbode4life@gmail.com>
2023-08-11 04:33:21 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
-/**
- * main - Starting Point
- *    
- * Return: Always 0 (Success)
- */
 
+/**
+ * main - Prints exactly "and that piece of art is useful" - Dora Korpar,
+ *         2015-10-19, followed by a new line, to the standard error.
+ *    
+ * Return: Always 1.
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
