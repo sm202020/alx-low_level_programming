8f5b0c74d46fa1beeda2879f939bd6f6648d7c0b
Sunday Mebawondu <akinbode4life@gmail.com>
2023-08-11 18:55:26 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main -starting point
- *
+ * main - starting point
  * Return: returns (0) on success
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		puts("is positive");
-	}
-	else if (n == 0)
-	{
-		puts("is zero");
-	}
-	else
-	{
-		puts("is negative");
-													}
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+puts("is positive");
+}
+else if (n == 0)
+{
+puts("is zero");
+}
+else
+{
+puts("is negative");
+return (0);
 }
