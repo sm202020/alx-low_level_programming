749c2c8819f2a5809d730828dbfa862ec33d8a70
Sunday Mebawondu <akinbode4life@gmail.com>
2023-08-11 21:06:12 +0000

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+int j = 0;
+
+while (i <= 99)
+{
+j = i + 1;
+while (j <= 99)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+
+putchar(' ');
+putchar(j / 10 + '0');
+
+putchar(j % 10 + '0');
+
+if (i != 98 || j != 99)
+
+{
+putchar(',');
+putchar(' ');
+
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
