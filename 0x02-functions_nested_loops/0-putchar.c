4795fe0437b4ef7420a7217474fcfe3649eff0bc
Sunday Mebawondu <akinbode4life@gmail.com>
2023-08-15 19:39:20 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,14 @@
 #include <time.h>
 /**
  * main - Entry point
- *  
+ *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
 
-	printf("_putchar\n");
+printf("_putchar\n");
 
-	return (0);
+return (0);
 }
