b8c1d67cecc17d56e574c88753d9e5ec112b308d
Sunday Mebawondu <akinbode4life@gmail.com>
2023-08-15 22:40:18 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,18 +24,18 @@ void times_table(void)
 	_putchar(32);
 	_putchar(d + '0');
 	_putchar(u + '0');
-	}	
+	}
 	else
 	{
 	if (y != 0)
-	{	
+	{
 	_putchar(44);
 	_putchar(32);
 	_putchar(32);
-	}	
-	_putchar(z + '0'); 
+	}
+	_putchar(z + '0');
 	}
 	}
-	_putchar('\n');				
+	_putchar('\n');
 	}
 }
