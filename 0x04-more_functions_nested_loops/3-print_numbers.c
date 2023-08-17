fdd75bfb364c981d9c5d34243419be1e45508698
Sunday Mebawondu <akinbode4life@gmail.com>
2023-08-17 19:17:10 +0000

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,15 @@
 /**
  * print_numbers - function that print all numbers from
  * 0 to 9
- * 
+ *
  * Return: returns nothing
  */
 void print_numbers(void)
 {
-	int number = 48;
-	for (number = 48; number < 58; number++)
-	{
-		_putchar(number);
-	}
-	_putchar(10);
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
 }
